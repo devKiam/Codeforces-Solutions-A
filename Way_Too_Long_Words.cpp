3b95522c9f2afc455c8f179c81aa4c07c40e15a3
nerdyKiam <45430833+nerdyKiam@users.noreply.github.com>
2019-11-12 20:27:52 +0600

    Create Way_Too_Long_Words.cpp

---
+++ Way_Too_Long_Words.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+main()
+{
+    int n;
+    string s;
+
+    cin>>n;
+    getchar();
+
+    for(int i=0; i<n; i++)
+    {
+        getline(cin,s);
+
+        int length = s.size();
+        if(length > 10)
+        {
+            cout<<s[0]<<length-2<<s[length-1]<<endl;
+        }
+        else{
+            cout<<s<<endl;
+        }
+    }
+}
