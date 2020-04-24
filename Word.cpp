51e6b3475b816c975cf349a8baa33af6b74db121
nerdyKiam <mdnuhoulalam@gmail.com>
2020-04-24 20:23:20 +0600

    Create Word.cpp

---
+++ Word.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string s;
+
+    while(cin>>s)
+    {
+        int UcaseCount = 0, LcaseCount = 0;
+
+        for(int i=0; i<s.size(); i++)
+        {
+            if(s[i]<97) UcaseCount++;
+            else LcaseCount++;
+        }
+
+
+        if(UcaseCount > LcaseCount){
+            transform(s.begin(), s.end(), s.begin(), ::toupper);
+            cout<<s<<endl;
+        }
+        else
+        {
+            transform(s.begin(), s.end(), s.begin(), ::tolower);
+            cout<<s<<endl;
+        }
+
+
+    }
+    return 0;
+}
