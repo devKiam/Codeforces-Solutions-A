f9540ef327cd788d3007c6f74ec40aa5184197f2
Md. Nuho-Ul Alam Kiam <mdnuhoulalam@gmail.com>
2020-07-13 02:49:13 +0600

    Create Anton and Letters.cpp

---
+++ Anton and Letters.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string s;
+
+    while(getline(cin,s))
+    {
+        set<char> st;
+
+        for(int i=0; i<s.size(); i++)
+        {
+            if(s[i]>=97 && s[i]<=122)
+            {
+                st.insert(s[i]);
+            }
+        }
+
+        int count = 0;
+
+        for(auto itr=st.begin(); itr!=st.end(); itr++)
+        {
+            count++;
+        }
+
+        cout<<count<<endl;
+    }
+}
