facaa3161b68116156799a418b6bfa5afb9bd993
Md. Nuho-Ul Alam Kiam <mdnuhoulalam@gmail.com>
2020-07-14 02:26:32 +0600

    Create New Password.cpp

---
+++ New Password.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    string s = "abcdefghijklmnopqrstuvwxyz";
+    int a,b;
+
+    while(cin>>a>>b)
+    {
+        for(int i=0; i<b; i++)
+        {
+            cout<<s[i];
+        }
+        
+        int diff = a-b;
+        
+        for(int i=0; i<b && diff>0; i++, diff--)
+        {
+            cout<<s[i];
+            if(diff-1 != 0 && i+1 == b) i=-1;
+        }
+        
+        cout<<endl;
+    }
+}
