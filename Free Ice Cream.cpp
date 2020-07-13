e8a75fb8c98c18a1757a34e1624e58b7cde4d571
Md. Nuho-Ul Alam Kiam <mdnuhoulalam@gmail.com>
2020-07-13 11:43:39 +0600

    Create Free Ice Cream.cpp

---
+++ Free Ice Cream.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    long long a,b;
+
+    while(cin>>a>>b)
+    {
+        long long dis = 0, ic = b;
+
+        for(long long i=0; i<a; i++)
+        {
+            char ch; cin>>ch;
+            long long x; cin>>x;
+
+            if(ch == '+') ic+=x;
+            else{
+                if(ic<x) dis++;
+                else{
+                    ic-=x;
+                }
+            }
+        }
+
+        cout<<ic<<" "<<dis<<endl;
+    }
+}
