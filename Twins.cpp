20672fea8299cfc0cd8462f211eba4e0ec12e3bc
Md. Nuho-Ul Alam Kiam <mdnuhoulalam@gmail.com>
2020-07-14 15:18:43 +0600

    Create Twins.cpp

---
+++ Twins.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int x;
+    
+    while(cin>>x)
+    {
+        vector<int> v;
+        int sum = 0;
+        
+        for(int i=0; i<x; i++)
+        {
+            int n; cin>>n; sum+=n;
+            v.push_back(n);
+        }
+        
+        sort(v.begin(), v.end());
+        
+        int sum2 = 0, count = 0;
+        
+        for(int i=v.size()-1; i>=0; i--)
+        {
+            if(sum2<=sum)
+            {
+                sum2+=v[i];
+                count++;
+                sum-=v[i];
+            }
+            else{
+                break;
+            }
+        }
+        
+        cout<<count<<endl;
+    }
+}
