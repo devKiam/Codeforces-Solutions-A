111c34db809f4e3ccd41864eb8d74e8148c80d93
Md. Nuho-Ul Alam Kiam <mdnuhoulalam@gmail.com>
2020-08-08 13:30:38 +0600

    math, pattern

---
+++ Rainbow Dash, Fluttershy and Chess Coloring.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+typedef long long ll;
+typedef unsigned long long ull;
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+   
+    int t; cin>>t;
+    while(t--)
+    {
+        int x; cin>>x;
+ 
+        cout<<(x/2)+1<<"\n";
+    }
+}
