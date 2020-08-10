2cf27e9084dff133261e1fba5735775a1ac35bce
Md. Nuho-Ul Alam Kiam <mdnuhoulalam@gmail.com>
2020-08-10 13:03:38 +0600

    math

---
+++ Suborrays.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+typedef long long ll;
+typedef unsigned long long ull;
+using namespace std;
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(0);
+    
+    int t; cin>>t;
+    while(t--)
+    {
+        int n; cin>>n;
+        for(int i=n; i>=1; i--)
+        {
+            cout<<i<<" "<<" ";
+        }
+        cout<<"\n";
+    }
+}
